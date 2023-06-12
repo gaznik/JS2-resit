e5d6c07526e67eecd9e39e34ec6ea480e72fda4a
Gazmend Nikqi <gazmendn93@gmail.com>
2023-06-12 21:36:12 +0200

    API url variables

---
+++ src/scripts/api/apiURL.mjs
@@ -0,0 +1,4 @@
+export const API_URL = "https://dummyjson.com"
+export const API_URL_USERS = "https://dummyjson.com/users";
+export const API_URL_PRODUCTS = "https://dummyjson.com/products";
+export const API_URL_LOGIN = "https://dummyjson.com/auth/login";
