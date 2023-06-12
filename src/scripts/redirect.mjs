ace17158ca5b9eab07539ce47dac39744856f689
Gazmend Nikqi <gazmendn93@gmail.com>
2023-06-12 21:35:42 +0200

    Redirection after user logged in

--- src/scripts/redirect.mjs
+++ src/scripts/redirect.mjs
@@ -1,1 +1,8 @@
-window.location.pathname = '/login.html';
+import { userIsLoggedIn } from "./api/userIsLoggedIn.mjs";
+
+
+    if (userIsLoggedIn()) {
+    window.location.pathname = '/products.html';
+    } else {
+    window.location.pathname = '/login.html';
+    }
